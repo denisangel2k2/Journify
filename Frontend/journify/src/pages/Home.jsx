5356d18a1d44d3b8f0bd2e06c231632007f86909
Denis Moldovan <denismoldovan1@gmail.com>
2024-04-16 01:33:07 +0300

    fetch journal

--- Frontend/journify/src/pages/Home.jsx
+++ Frontend/journify/src/pages/Home.jsx
@@ -5,39 +5,51 @@ import { Modal } from 'react-bootstrap';
 import UserContainer from '../components/home-components/UserContainer';
 import TableHistory from '../components/home-components/TableHistory';
 import SearchBar from '../components/home-components/SearchBar';
-import { useLocalStorage } from '../providers/AuthProvider';
+import { useAuth, useLocalStorage } from '../providers/AuthProvider';
 
 export const Home = () => {
   const [cells, setCells] = useLocalStorage('cells', []);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedCell, setSelectedCell] = useState({});
   const [currentSelectedSong, setCurrentSelectedSong] = useState(null);
+  const { token, userInfo } = useAuth();
 
+  const fetchCells = async () => {
+    try {
+      const journal = await fetch('http://localhost:8888/journal', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `${token}`
+        },
+        body: JSON.stringify({
+          email: userInfo.email,
+          spotify_id: userInfo.id
+        })
+      });
 
-  const fetchCells = () => {
-    return [
-      { id: 1, img: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg", "song": "" },
-      { id: 2, img: "", "song": "" },
-      { id: 3, img: "", "song": "" },
-      { id: 3, img: "", "song": "" },
-      { id: 3, img: "", "song": "" },
-      { id: 3, img: "", "song": "" },
-      { id: 3, img: "", "song": "" },
-      { id: 3, img: "", "song": "" },
-      { id: 3, img: "", "song": "" },
-      { id: 3, img: "", "song": "" },
-      { id: 3, img: "", "song": "" },
-      { id: 3, img: "", "song": "" },
-    ];
+      if (!journal.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await journal.json();
+      setCells(data);
+    } catch (error) {
+      console.log('Error fetching cells:', error);
+    }
   };
-
   useEffect(() => {
-    const fetchedCells = fetchCells();
-    setCells(fetchedCells);
-  }, []);
+    console.log('HEREEEE' + !cells)
+    fetchCells();
+  }, [token, userInfo]);
 
   const saveModalChanges = () => {
+    setIsModalVisible(false);
+    const newCells = cells;
+    newCells.questions[selectedCell.index].song = currentSelectedSong;
+    //fetch info from the server regarding the classification of the song and update the cell with the image and classification
+    
 
+    setCells(newCells);
   }
 
   const handleClickOnCell = (item) => {
@@ -52,10 +64,10 @@ export const Home = () => {
     <>
       <Modal show={isModalVisible}>
         <Modal.Header>
-          <Modal.Title>{selectedCell.id}</Modal.Title>
+          <Modal.Title>{selectedCell.index + 1}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>Modal body text goes here.</p>
+          <p>{selectedCell.question}</p>
           <SearchBar setCurrentSelectedSong={setCurrentSelectedSong}></SearchBar>
         </Modal.Body>
         <Modal.Footer>
@@ -67,9 +79,9 @@ export const Home = () => {
           <UserContainer />
           <div className="main-container">
             <div className="table-container">
-              {cells.map(cell => (
+              {cells?.questions && cells.questions.map(cell => (
                 <JournalCell
-                  key={cell.id}
+                  key={cell.index}
                   item={cell}
                   onClick={() => handleClickOnCell(cell)}
                 />
@@ -80,8 +92,6 @@ export const Home = () => {
         </div>
         <TableHistory />
       </div>
-
-
     </>
 
   );
